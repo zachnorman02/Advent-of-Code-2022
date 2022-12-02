0c1160d3f66f607120f917ef1380e77e31b6585b
Zach Norman <zachnorman02@gmail.com>
2022-12-02 00:39:13 -0500

    Finish Day 2 (naive)

--- day2/day2.py
+++ day2/day2.py
@@ -13,6 +13,31 @@ def rps(p1, p2):
         return scores[p2]
     return 6 + scores[p2]
 
+def correct_rps(p1, p2):
+    scores = {
+        'A': 1,
+        'B': 2,
+        'C': 3
+    }
+    if p1 == 'A':
+        if p2 == 'X':
+            return scores['C']
+        if p2 == 'Y':
+            return scores['A']+3
+        return scores['B']+6
+    if p1 == 'B':
+        if p2 == 'X':
+            return scores['A']
+        if p2 == 'Y':
+            return scores['B']+3
+        return scores['C']+6
+    if p1 == 'C':
+        if p2 == 'X':
+            return scores['B']
+        if p2 == 'Y':
+            return scores['C']+3
+        return scores['A']+6
+
 def get_file():
     with open('day2input.txt') as f:
         file_text = f.readlines()
@@ -21,4 +46,5 @@ def get_file():
 if __name__ == '__main__':
     file_text = get_file()
     strats = [line.split(' ') for line in file_text]
-    print(strats)
+    scores = [correct_rps(s[0], s[1][:-1]) if '\n' in s[1] else correct_rps(s[0], s[1]) for s in strats]
+    print(sum(scores))
